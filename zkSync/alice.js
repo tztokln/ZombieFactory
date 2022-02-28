3b98c2c67cffd3b8fbbe2b4ebd3e28fbf8cf88cb
elena <tztokln@gmail.com>
2022-02-28 13:46:37 -0500

    change Alice

--- zkSync/alice.js
+++ zkSync/alice.js
@@ -2,15 +2,15 @@
     const ethers = require('ethers')
     const zksync = require('zksync')
     const utils = require('./utils')
-    const token = 'ETH'
-    const amountToDeposit = '0.05'
-    const amountToTransfer = '0.02'
-    const amountToWithdraw = '0.002'
+    const token = 'ETH' // Update this line
+    const amountToDeposit = '0.05' // Update this line
+    const amountToTransfer = '0.02' // Update this line
+    const amountToWithdraw = '0.002' // Update this line
   
     const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
     const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
     console.log('Creating a new Rinkeby wallet for Alice')
-    const aliceRinkebyWallet = new ethers.Wallet(process.env.ALICE_PRIVATE_KEY, ethersProvider) // Account #78
+    const aliceRinkebyWallet = new ethers.Wallet(process.env.ALICE_PRIVATE_KEY, ethersProvider)
     console.log(`Alice's Rinkeby address is: ${aliceRinkebyWallet.address}`)
     const aliceInitialRinkebyBalance = await aliceRinkebyWallet.getBalance()
     console.log(`Alice's initial balance on Rinkeby is: ${ethers.utils.formatEther(aliceInitialRinkebyBalance)}`)
@@ -28,9 +28,7 @@
     await utils.transfer(aliceZkSyncWallet, process.env.BOB_ADDRESS, amountToTransfer, transferFee, token, zksync, ethers)
   
     console.log('Withdrawing')
-    // Start here
-    const withdrawalFee = utils.getFee('Withdraw', aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
+    const withdrawalFee = await utils.getFee('Withdraw', aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
     await utils.withdrawToEthereum(aliceZkSyncWallet, amountToWithdraw, withdrawalFee, token, zksync, ethers)
-  
   })()
   
